1eea5780efe01fb1d4c80ca197094227901a663f
Naziya Parveen <166731080+Naziya007@users.noreply.github.com>
2026-01-20 14:35:41 +0530

    Add dummy build script for Render deployment

--- frontend/my-react-app/src/api.js
+++ frontend/my-react-app/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://pos-project-goif.onrender.com", // replace with your backend URL
+  baseURL: "https://pos-project-5.onrender.com", // replace with your backend URL
 });
 // Attach token automatically
 API.interceptors.request.use((req) => {
